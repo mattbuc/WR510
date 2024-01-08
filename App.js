ccca43b95faf8c3416a0176eb7ab432a93cbdbaf
Matthias Buczek <matt-buc@orange.fr>
2024-01-08 15:27:13 +0100

    feat: add screens

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.box, { backgroundColor: 'blue' }]}>Je suis blue</Text>
+      <Text style={[styles.box, { backgroundColor: 'yellow' }]}>Je suis yellow</Text>
+      <Text style={[styles.box, { backgroundColor: 'red' }]}>Je suis green</Text>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
@@ -16,5 +19,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    color: 'white',
+    borderRadius: 10,
+    width: 200,
+    height: 100,
+    textAlign: 'center',
+    margin: 20,
   },
 });
